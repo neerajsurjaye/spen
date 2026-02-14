4dcdfd68ef5571bdf3c62dfd7414fb9027b25b0e
Neeraj Surjaye <neerajsurjaye@gmail.com>
2026-02-14 16:46:17 +0530

    Well two triangles --Neeraj

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"runtime"
 	"strings"
-	"time"
 
 	"github.com/go-gl/gl/all-core/gl"
 	"github.com/go-gl/glfw/v3.3/glfw"
@@ -16,37 +15,37 @@ const(
 	height = 800
 )
 
-var vertexShaderSource = `
-	#version 410
-	in vec3 vp;
-	void main(){
-		gl_Position = vec4(vp, 1.0);
-	}
-` + "\x00"
-
-var fragmentShaderSource = `
-	#version 410
-	out vec4 frag_colour;
-	void main(){
-		frag_colour = vec4(1,1,1,1);
-	}
-` + "\x00"
-
 
 func init(){
 	runtime.LockOSThread()
 }
 
 func main() {
-	fmt.Println("Stargin SPEN")
+	fmt.Println("Starting SPEN")
 
 	window := initGlfw()
 	defer glfw.Terminate()
 
-	prog := initOpenGL()
-	vao := makeVao(triangle)
+	if err := gl.Init(); err != nil{
+		log.Fatalln(err)
+	}
+
+	vao := []uint32{}
+	prog := []uint32{}
+
+	triagVao, progVao := makeTriangle()
+
+	vao = append(vao, triagVao)
+	prog = append(prog, progVao)
+
+	triagVao, progVao = makeTriangle()
+
+	vao = append(vao, triagVao)
+	prog = append(prog, progVao)
+
 
 	for !window.ShouldClose(){
+
 		draw(vao, window, prog)
 	}
 
@@ -74,84 +73,36 @@ func initGlfw() *glfw.Window{
 	return window
 }
 
-func initOpenGL() uint32{
 
-	if err := gl.Init(); err != nil{
-		log.Fatalln(err)
-	}
-
-	//Just Logs
-	version := gl.GoStr(gl.GetString(gl.VERSION))	
-	renderer := gl.GoStr(gl.GetString(gl.RENDERER))
-	vendor := gl.GoStr(gl.GetString(gl.VENDOR))
-
-	log.Println("OpenGL Version :", version)
-	log.Println("Renderer       :", renderer)
-	log.Println("Vendor         :", vendor)
-
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil{
-		panic(err)
-	}
-
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil{
-		panic(err)
-	}
-
-	prog := gl.CreateProgram()
-	gl.AttachShader(prog, vertexShader)
-	gl.AttachShader(prog, fragmentShader)
-	gl.LinkProgram(prog)
-	return prog
-
-}
-
-var prevTime = time.Now()
-
-func draw(vao uint32, window *glfw.Window, program uint32){
+func draw(vao []uint32, window *glfw.Window, program []uint32){
 	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-	gl.UseProgram(program)
 
-	gl.BindVertexArray(vao)
-	gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/ 3))
+
+	
+
+	
+	for	i := 0; i < len(vao) ; i++{
+		gl.UseProgram(program[i])
+		gl.BindVertexArray(vao[i])
+
+		if i == 0{
+			offSetLoc := gl.GetUniformLocation(program[0], gl.Str("uOffset\x00"))
+			gl.Uniform3f(offSetLoc, 0, 1 , 0)
+		} else if i == 1{
+			offSetLoc1 := gl.GetUniformLocation(program[1], gl.Str("uOffset\x00"))
+			gl.Uniform3f(offSetLoc1, 0, -1 , 0)
+		}
+
+		gl.DrawArrays(gl.TRIANGLES, 0, int32(len(triangle)/ 3))
+	}
 
 	window.SwapBuffers()
 	glfw.PollEvents()
-
-	log.Println("Time Diff" , time.Now().Sub(prevTime))
-	prevTime = time.Now()
 }
 
-var triangle []float32 = []float32{
-	0, 0.5,0,
-	-0.5, -0.5, 0,
-	0.5, -0.5, 0,
-}
-
-func makeVao(points []float32) uint32{
-	var vbo uint32 //Vertex buffer object
-
-	//Creates a GPU buffer
-	gl.GenBuffers(1, &vbo)
-	//Any operation on ArrayBuffer refers to this VBO.
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData( 					//Push Data to GPU
-		gl.ARRAY_BUFFER,			//Upload as array buffer 
-		4*len(points),				//Allocate this much mem in gpu. 4 is size of float in bytes. So 4 * number of elements.
-		gl.Ptr(points),				//Copies to GPU by taking its pointer
-		gl.STATIC_DRAW)				//Tells data wont change very much
-
-	var vao uint32
-	gl.GenVertexArrays(1, &vao)									//Creates Vertex Array object
-	gl.BindVertexArray(vao)										//Makes it the active VAO
-	gl.EnableVertexAttribArray(0)								//Attribute loc as 0 **Research more on it**
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)							//Binds VBO as as currnt array buffer
-	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)		//0 attributes location. Components per vertex(x,y,z). Then datatype, Normalize false correct for floats,Stride 0 = open gl caculates itself , offset nil start at beginning of buffer.
-
-	return vao
-}
-
+/**
+	Compiles shader
+*/
 func compileShader(source string, shaderType uint32) (uint32, error){
 	shader := gl.CreateShader(shaderType)
 
@@ -176,3 +127,88 @@ func compileShader(source string, shaderType uint32) (uint32, error){
 	return shader, nil
 }
 
+// returns vao, prog
+func makeTriangle () (uint32, uint32){
+	vao := makeTrianglVao(triangle)
+	prog := initTriagnleProgram(vertexShaderSource, fragmentShaderSource)
+
+	return vao, prog
+}
+
+var triangle []float32 = []float32{
+	0, 0.5 ,0,
+	-0.5, -0.5, 0,
+	0.5, -0.5, 0,
+}
+
+var vertexShaderSource = `
+	#version 410
+	layout (location = 0) in vec3 vp;
+	uniform vec3 uOffset;
+
+	void main(){
+		gl_Position = vec4(vp + uOffset, 1.0);
+	}
+` + "\x00"
+
+var fragmentShaderSource = `
+	#version 410
+	out vec4 frag_colour;
+	void main(){
+		frag_colour = vec4(1,1,1,1);
+	}
+` + "\x00"
+
+func makeTrianglVao(points []float32) uint32{
+	var vbo uint32 //Vertex buffer object
+
+	fmt.Println("==Debug==")
+
+
+	// VBO tells take this much of memory and copy this data to gpu
+	//Creates a GPU buffer
+	gl.GenBuffers(1, &vbo)
+
+	//Any operation on ArrayBuffer refers to this VBO.
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
+
+	gl.BufferData( 					//Push Data to GPU
+		gl.ARRAY_BUFFER,			//Upload as array buffer 
+		4*len(points),				//Allocate this much mem in gpu. 4 is size of float in bytes. So 4 * number of elements.
+		gl.Ptr(points),				//Copies to GPU by taking its pointer
+		gl.STATIC_DRAW)				//Tells data wont change very much
+
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)									//Creates Vertex Array object
+	gl.BindVertexArray(vao)										//Makes it the active VAO
+	gl.EnableVertexAttribArray(0)								//Attribute loc as 0 **Research more on it**
+	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)							//Binds VBO as as currnt array buffer
+	gl.VertexAttribPointer(0, 3, gl.FLOAT, false, 0, nil)		//0 attributes location. Components per vertex(x,y,z). Then datatype, Normalize false correct for floats,Stride 0 = open gl caculates itself , offset nil start at beginning of buffer.
+
+	return vao
+}
+
+
+func initTriagnleProgram(vertexShaderCode string, fragmentShaderCode string) uint32{
+
+	if err := gl.Init(); err != nil{
+		log.Fatalln(err)
+	}
+
+	vertexShader, err := compileShader(vertexShaderCode, gl.VERTEX_SHADER)
+	if err != nil{
+		panic(err)
+	}
+
+	fragmentShader, err := compileShader(fragmentShaderCode, gl.FRAGMENT_SHADER)
+	if err != nil{
+		panic(err)
+	}
+
+	prog := gl.CreateProgram()
+	gl.AttachShader(prog, vertexShader)
+	gl.AttachShader(prog, fragmentShader)
+	gl.LinkProgram(prog)
+	return prog
+}
